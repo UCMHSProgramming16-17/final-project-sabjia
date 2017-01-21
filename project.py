ab1f91d829be96a66f0cad5fa3d7c99688a81a4c
Sabina Jia <sjia@ucvts.org>
2017-01-21 03:42:32 +0000

    changed the csv file that I was using, created four visuals

--- project.py
+++ project.py
@@ -5,23 +5,35 @@ import numpy as np
 import bokeh
 
 # create file
-df = pd.read_csv('population.csv')
+df = pd.read_csv('cereal.csv')
 
-# first graph: bar graph of the number of total asian population in each borough
-from bokeh.charts import Bar, output_file, save
-kayleen = Bar(df, 'Borough', values='Total Asian Only', title='Asian Population in NYC Boroughs (2010)', color='#DEAAF8')
+# first graph: Box Plot of calories vs. fat in cereal
+from bokeh.charts import BoxPlot, output_file, save
+kayleen = BoxPlot(df, label='calories', values='fat', title="Calories vs. Fat", color='calories')
 
 output_file('kayleen.html')
-save(kayleen)           #save file!!!
+save(kayleen)
 
-# second graph: pie chart of how many chinese people in each borough
-from bokeh.charts import Donut, output_file, save
-
-hoshi = Donut(df, label=['Borough','Total Asian - Chinese and Taiwanese'], values='Total Asian - Chinese and Taiwanese', text_font_size='7pt', 
-title='Total Chinese and Taiwanese in NYC Boroughs (2010)', color=['#A4EED7','#C7EEA4','#FBF37C','#F9AAA2','#CAA2F9'])
-
+# second graph: Scatter plot of carbs vs. sugars
+from bokeh.charts import Scatter, output_file, save
+hoshi = Scatter(df, x='carbo', y='sugars', title='Carbs vs. Sugars', xlabel='Total Carbohydrates per serving (g)', ylabel='Total Sugar per serving (g)', color='#CAA2F9')
 
 output_file('hoshi.html')
-save(hoshi)             #save the file!
+save(hoshi)
 
-# third graph: 
+# third graph: bar graph of average sodium for fat values
+from bokeh.charts import Bar, output_file, save
+suga = Bar(df, 'fat', values='sodium', agg='mean', title='Average Sodium Levels vs. Fat Levels', color='#C8FCB3')
+
+output_file('suga.html')
+save(suga)
+
+# fourth graph: grouped bar graph of average sodium for calories, grouped on shelf value
+from bokeh.charts import Bar, output_file, save
+from bokeh.palettes import Pastel1
+pal = Pastel1[3]
+
+akaashi = Bar(df, 'calories', values='sodium', agg='mean', group='shelf', title='Average Sodium level per Calorie Amount, grouped by shelf value', color='shelf', palette=pal, background_fill_color='#FFFEBC')
+
+output_file('akaashi.html')
+save(akaashi)
